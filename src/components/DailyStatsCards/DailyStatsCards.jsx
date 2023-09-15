2d488857c262fabbf525b3e08023551b29c1d0f0
Vladyslav <dyadyavadya308@gmail.com>
2023-09-15 12:09:13 +0300

    features/added new file

---
+++ src/components/DailyStatsCards/DailyStatsCards.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const DailyStatsCards = ({props}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+DailyStatsCards.propTypes = {
+
+}
+
+export default DailyStatsCards
+
