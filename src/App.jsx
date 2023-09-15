01148eee6c9944f2298767dcbc76efae5c13cd32
Vladyslav <dyadyavadya308@gmail.com>
2023-09-15 17:11:46 +0300

    removed some strings from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import SharedLayout from './components/SharedLayout/SharedLayout';
 // import SharedLayout from './components/SharedLayout/SharedLayout';
 
 const Home = lazy(() => import('../src/pages/Home/Home'));
-import DailyStatsCards from './components/DailyStatsCards/DailyStatsCards';
 
 const test = import.meta.env.VITE_API_TEST;
 
@@ -13,64 +12,11 @@ function App() {
   //   console.log(test);
   return (
     <>
-      <ul
-        style={{
-          listStyle: 'none',
-          padding: '10px',
-          display: 'grid',
-          gridTemplateColumns: "1fr 1fr",
-          justifyContent: 'space-around',
-          gap: '8px',
-          backgroundColor: 'black',
-          border: '1px solid red',
-        }}
-      >
-        <li>
-          <DailyStatsCards
-            icon="running-man"
-            label=" The rest of sports"
-            keyValue="2200"
-            fill="false"
-            border="default"
-          />
-        </li>
-
-        <li>
-          <DailyStatsCards
-            icon="running-man"
-            label=" The rest of sports"
-            keyValue="2200"
-            fill="false"
-            border="default"
-          />
-        </li>
-
-        <li>
-          <DailyStatsCards
-            icon="running-man"
-            label=" The rest of sports"
-            keyValue="2200"
-            fill="false"
-            border="default"
-          />
-        </li>
-
-        <li>
-          <DailyStatsCards
-            icon="running-man"
-            label=" The rest of sports"
-            keyValue="2200"
-            fill="false"
-            border="default"
-          />
-        </li>
-      </ul>
-
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
         </Route>
-      </Routes> */}
+      </Routes>
     </>
   );
 }
