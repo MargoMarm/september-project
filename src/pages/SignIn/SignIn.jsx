aed170957ef6f41bbfc75e45095b480709c818b9
MargoMarm <margogerasimenko@gmail.com>
2023-09-15 17:39:55 +0200

    add routes for pages

---
+++ src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,7 @@
+const SignIn = () => {
+	return (
+		<p>THIS IS PAGE SIGN IN</p>
+	)
+}
+
+export default SignIn
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,5 @@
+const SignUp = () => {
+  return <p>THIS IS PAGE SIGN UP</p>;
+};
+
+export default SignUp;
