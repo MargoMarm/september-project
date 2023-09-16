1ff599b5e3c214b8a521210867e8a7e9594c0054
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-09-16 19:45:52 +0300

    add icon

--- src/components/DiscriptionText/DiscriptionText.styled.jsx
+++ src/components/DiscriptionText/DiscriptionText.styled.jsx
@@ -1,26 +1,26 @@
 import styled from '@emotion/styled';
 import { colors, mq } from '../../utils';
 
+export const Container = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export const Icon = styled.svg`
+  width: 24px;
+  height: 24px;
+`;
+
 export const Text = styled.p`
   color: ${colors.textWhite03};
   font-family: Roboto;
   font-size: 14px;
   font-style: normal;
-  font-weight: 400;
   line-height: 1.3;
 
-  ${mq.mobile} {
-    width: 303px;
-  }
-
   ${mq.tablet} {
-    width: 561px;
     font-size: 16px;
 
     line-height: 1.5;
-  }
-
-  ${mq.desktop} {
-    width: 358px;
   }
 `;
