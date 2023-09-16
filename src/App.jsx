d36626bd1ca55ea6caaff8a150844a0e80987e70
MargoMarm <margogerasimenko@gmail.com>
2023-09-16 18:25:41 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,59 +13,6 @@ function App() {
   //   console.log(test);
   return (
     <>
-      <ul
-        style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(2, 1fr)',
-          gap: '16px',
-          width: '390px',
-          backgroundColor: 'black',
-        }}
-      >
-        <li>
-          <DailyStatsCards
-            label="Daily calorie intake"
-            fill="true"
-            keyValue="2200"
-            icon="fire"
-          />
-        </li>
-        <li>
-          <DailyStatsCards
-            label="Daily norm of sports"
-            fill="true"
-            keyValue="110 min"
-            icon="dumbbell"
-          />
-        </li>
-        <li>
-          <DailyStatsCards
-            label="Сalories consumed"
-            keyValue="2300"
-            icon="apple"
-          />
-        </li>
-        <li>
-          <DailyStatsCards label="Сalories burned" keyValue="855" icon="fire" />
-        </li>
-        <li>
-          <DailyStatsCards
-            label="The rest of the calories"
-            keyValue="i`m running"
-            border="green"
-            icon="fire"
-          />
-        </li>
-        <li>
-          <DailyStatsCards
-            label="The rest of sports"
-            keyValue="i`m running"
-            border="red"
-            icon="fire"
-          />
-        </li>
-      </ul>
-
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
