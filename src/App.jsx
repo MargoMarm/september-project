e7d53580ef99ace68749bf8672a357723c22cfd5
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-09-17 15:29:57 +0300

    update
    create exercises page
    import to app
    update products page

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ const Home = lazy(() => import('../src/pages/Home/Home'));
 const SignIn = lazy(() => import('../src/pages/SignIn/SignIn'));
 const SignUp = lazy(() => import('../src/pages/SignUp/SignUp'));
 const Products = lazy(() => import('../src/pages/Products/Products'));
+const Exercises = lazy(() => import('../src/pages/Exercises/Exercises'));
 
 const test = import.meta.env.VITE_API_TEST;
 
 function App() {
-	//   console.log(test);
+  //   console.log(test);
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
@@ -20,6 +21,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/exercises" element={<Exercises />} />
       </Route>
     </Routes>
   );
