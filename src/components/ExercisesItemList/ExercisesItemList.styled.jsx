d6dc23a9f15a6f38a2105eb41ed6cee77edd9779
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-09-17 15:21:32 +0300

    update Exercises
    update Exercises list

---
+++ src/components/ExercisesItemList/ExercisesItemList.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+// import { colors, mq } from '../../utils/index';
+
+export const ExercisesItemList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 16px;
+  margin-top: 40px;
+`;
