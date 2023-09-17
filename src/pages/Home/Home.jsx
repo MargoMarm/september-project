8f6baa549026e460a55eb0cb3b33d482b20b0e06
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-09-17 20:15:40 +0300

    delete some trash from Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,35 +1,7 @@
 import CustomNavLink from '../../components/CustomNavLink/CustomNavLink';
 import MainTitle from '../../components/MainTitle/MainTitle';
-import ProductsOrExercisesItem from '../../components/ProductsOrExercisesItem/ProductsOrExercisesItem';
 import Header from '../../components/headersComp/Header/Header';
 import { LinkList, Wrapper } from './Home.styled';
-
-const product = {
-  _id: {
-    $oid: '5d51694902b2373622ff5773',
-  },
-  weight: 100,
-  calories: 340,
-  category: 'dairy',
-  title: 'Salads Belaya Dacha Delicate root',
-  groupBloodNotAllowed: {
-    1: true,
-    2: true,
-    3: false,
-    4: false,
-  },
-};
-
-const exercise = {
-  bodyPart: 'waist',
-  equipment: 'body weight',
-  gifUrl:
-    'https://res.cloudinary.com/ditdqzoio/image/upload/v1687127066/exercises/0001.gif',
-  name: '3/4 sit-upqweewqewqqweqwewqewqewqe',
-  target: 'abs',
-  burnedCalories: 220,
-  time: 3,
-};
 
 const Home = () => {
   return (
@@ -45,7 +17,6 @@ const Home = () => {
             <CustomNavLink text={'Sign in'} to={'/signin'} />
           </li>
         </LinkList>
-        <ProductsOrExercisesItem page={'exercise'} data={exercise} />
       </Wrapper>
     </>
   );
