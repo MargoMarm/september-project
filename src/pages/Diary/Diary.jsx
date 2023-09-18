39dc2e79eb48f416145cfc781e115fcf3b5fd406
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-09-18 19:46:17 +0300

    added some styles

--- src/pages/Diary/Diary.jsx
+++ src/pages/Diary/Diary.jsx
@@ -2,23 +2,20 @@ import DairyStatisticList from '../../components/DiaryStatisticsList/DiaryStatis
 import Title from '../../components/Title/Title';
 import DayDiaryProductsOrExercises from '../../components/DayDiaryProductsOrExercises/DayDiaryProductsOrExercises';
 
-import {
-  DiaryPageContainer,
-  ExclamationSvg,
-  ExclamationText,
-} from './Diary.styled';
+import { DiaryPageContainer } from './Diary.styled';
 
-import sprite from '../../assets/sprite.svg';
-import DescriptionText from '../../components/DiscriptionText/DescriptionText';
+import DescriptionText from '../../components/DescriptionText/DescriptionText';
+import { mg } from '../../utils';
 
 const Diary = () => {
   return (
     <DiaryPageContainer>
-      <Title text={'Diary'} margin={40} />
+      <Title text={'Diary'} margin={mg} />
       <DairyStatisticList />
       <DescriptionText
         text="Record all your meals in a calorie diary every day. This will help me be
         aware of my nutrition and make me responsible for my choices."
+        width={{ tablet: 593, desktop: 390 }}
       />
       <DayDiaryProductsOrExercises products={[]} marginBottom={40} />
       <DayDiaryProductsOrExercises products={[]} isDayProducts />
--- src/pages/Diary/Diary.styled.jsx
+++ src/pages/Diary/Diary.styled.jsx
@@ -1,30 +1,6 @@
 import styled from '@emotion/styled';
-import { colors, mq } from '../../utils';
 
 export const DiaryPageContainer = styled.div`
   display: flex;
   flex-direction: column;
-`;
-
-export const ExclamationText = styled.p`
-  display: flex;
-  margin: 20px 0 40px 0;
-
-  font-size: 14px;
-  line-height: 1.29;
-
-  color: ${colors.textWhite03};
-
-  ${mq.tablet} {
-    width: 593px;
-
-    font-size: 16px;
-    line-height: 1.5;
-  }
-`;
-
-export const ExclamationSvg = styled.svg`
-  max-width: 24px;
-  max-height: 24px;
-  margin-right: 8px;
 `;
