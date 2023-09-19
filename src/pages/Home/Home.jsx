d2cb647e5e1f10a1ab405a4f7aae6dd87e31016d
unknown <p.burchenkov@ukr.net>
2023-09-19 14:58:20 +0300

    a little fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,25 @@
+import CustomNavLink from '../../components/CustomNavLink/CustomNavLink';
+import MainTitle from '../../components/MainTitle/MainTitle';
+import Header from '../../components/headersComp/Header/Header';
+import { LinkList, Wrapper } from './Home.styled';
+
 const Home = () => {
-  return <p>Product page</p>;
+  return (
+    <>
+      <Wrapper>
+        <Header />
+        <MainTitle />
+        <LinkList>
+          <li>
+            <CustomNavLink isorange={'true'} text={'Sign up'} to={'/signup'} />
+          </li>
+          <li>
+            <CustomNavLink text={'Sign in'} to={'/signin'} />
+          </li>
+        </LinkList>
+      </Wrapper>
+    </>
+  );
 };
 
 export default Home;
