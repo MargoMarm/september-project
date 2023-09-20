a1833a3578872f1373310543a3a62da17fb4967d
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-09-20 17:20:55 +0300

    added selectors to products and exercises redux

---
+++ src/redux/exercises/selectors.js
@@ -0,0 +1,4 @@
+export const getIsLoadingExercises = state => state.isLoading;
+
+export const getErrorExercises = state => state.error;
+
---
+++ src/redux/products/selectors.js
@@ -0,0 +1,7 @@
+export const getDiaryProducts = state => state.products;
+
+export const getDiaryExercises = state => state.exercises;
+
+export const getIsLoadingProducts = state => state.isLoading;
+
+export const getErrorProducts = state => state.error;
