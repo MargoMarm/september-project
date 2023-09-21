0af679fbda7b1e688dd0d0d8e8047d41874c8016
Anzhela Bazalinska <anzhela.bazalinskaya@gmail.com>
2023-09-21 22:13:22 +0300

    added modal add-product

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -5,8 +5,14 @@ import { FlexWrapper, ProductPageContainer } from './Products.styled';
 import productsList from '../../data/products.json';
 import Scrollbar from '../../components/Scrollbar';
 import ProductsOrExercisesItem from '../../components/ProductsOrExercisesItem/ProductsOrExercisesItem';
+import Modal from '../../components/Modal/Modal';
+import AddProductForm from '../../components/AddProductForm/AddProductForm';
+import { useState } from 'react';
 
 const Products = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toggleModal = () => setIsModalOpen(state => !state);
+
   return (
     <ProductPageContainer>
       <FlexWrapper>
@@ -18,13 +24,23 @@ const Products = () => {
           {productsList.map((product, i) => {
             if (i < 20) {
               return (
-                <ProductsOrExercisesItem
-                  key={product._id.$oid}
-                  page="product"
-                  data={product}
-                />
+                <>
+                  <ProductsOrExercisesItem
+                    key={product._id.$oid}
+                    page="product"
+                    data={product}
+                    openModal={toggleModal}
+                  />
+
+                  {isModalOpen && (
+                    <Modal openModal={toggleModal}>
+                      <AddProductForm closeModal={toggleModal} data={product} />
+                    </Modal>
+                  )}
+                </>
               );
             }
+
             return null;
           })}
         </ProductsOrExercisesContainer>
