6e4d59e7e96e9217590927cb8415c19120dd2bab
unknown <p.burchenkov@ukr.net>
2023-09-21 10:55:02 +0300

    universal-container

--- src/components/ProductOrExerciseContainer/ProductOrExerciseContainer.jsx
+++ src/components/ProductOrExerciseContainer/ProductOrExerciseContainer.jsx
@@ -1,26 +1,5 @@
-import PropTypes from 'prop-types';
 import { Container } from './ProductOrExerciseContainer.styled';
-import ProductsOrExercisesItem from '../../components/ProductsOrExercisesItem/ProductsOrExercisesItem';
 
-export default function ProductsOrExercisesContainer({ list, ...props }) {
-  return (
-    <Container {...props}>
-      {list.map((product, i) => {
-        if (i < 20) {
-          return (
-            <ProductsOrExercisesItem
-              key={product._id.$oid}
-              page="product"
-              data={product}
-            />
-          );
-        }
-        return null;
-      })}
-    </Container>
-  );
+export default function ProductsOrExercisesContainer({ children, ...props }) {
+  return <Container {...props}>{children}</Container>;
 }
-
-ProductsOrExercisesContainer.propTypes = {
-  list: PropTypes.array,
-};
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -4,6 +4,7 @@ import ProductsFilter from '../../components/ProductsFilter/ProductsFilter';
 import { FlexWrapper, ProductPageContainer } from './Products.styled';
 import productsList from '../../data/products.json';
 import Scrollbar from '../../components/Scrollbar';
+import ProductsOrExercisesItem from '../../components/ProductsOrExercisesItem/ProductsOrExercisesItem';
 
 const Products = () => {
   return (
@@ -13,7 +14,20 @@ const Products = () => {
         <ProductsFilter />
       </FlexWrapper>
       <Scrollbar width={{ dt: '868' }}>
-        <ProductsOrExercisesContainer list={productsList} />
+        <ProductsOrExercisesContainer>
+          {productsList.map((product, i) => {
+            if (i < 20) {
+              return (
+                <ProductsOrExercisesItem
+                  key={product._id.$oid}
+                  page="product"
+                  data={product}
+                />
+              );
+            }
+            return null;
+          })}
+        </ProductsOrExercisesContainer>
       </Scrollbar>
     </ProductPageContainer>
   );
