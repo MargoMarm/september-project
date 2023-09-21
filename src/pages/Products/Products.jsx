c6b62431f9ec6a4dd5530a46269ffafd1e8efbc9
Anzhela Bazalinska <anzhela.bazalinskaya@gmail.com>
2023-09-21 12:19:59 +0300

    Update Products.jsx

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -4,14 +4,8 @@ import Title from '../../components/Title/Title';
 import products from '../../data/products.json';
 import ProductsFilter from '../../components/ProductsFilter/ProductsFilter';
 import { FlexWrapper, ProductPageContainer } from './Products.styled';
-import { useState } from 'react';
-import AddProductForm from '../../components/AddProductForm/AddProductForm';
-import Modal from '../../components/Modal/Modal';
 
 const Products = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const toggleModal = () => setIsModalOpen(state => !state);
-
   return (
     <ProductPageContainer>
       <FlexWrapper>
@@ -27,14 +21,7 @@ const Products = () => {
                   key={product._id.$oid}
                   page="product"
                   data={product}
-                  openModal={toggleModal}
                 />
-
-                {isModalOpen && (
-                  <Modal openModal={toggleModal}>
-                    <AddProductForm closeModal={toggleModal} data={product} />
-                  </Modal>
-                )}
               </>
             );
           }
