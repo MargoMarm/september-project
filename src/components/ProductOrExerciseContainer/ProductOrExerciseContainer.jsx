3fb17c9284a6ea069759bce077db802b218211a6
unknown <p.burchenkov@ukr.net>
2023-09-21 10:22:16 +0300

    fix

--- src/components/ProductOrExerciseContainer/ProductOrExerciseContainer.jsx
+++ src/components/ProductOrExerciseContainer/ProductOrExerciseContainer.jsx
@@ -2,26 +2,22 @@ import PropTypes from 'prop-types';
 import { Container } from './ProductOrExerciseContainer.styled';
 import ProductsOrExercisesItem from '../../components/ProductsOrExercisesItem/ProductsOrExercisesItem';
 
-import Scrollbar from '../Scrollbar/Scrollbar';
-
 export default function ProductsOrExercisesContainer({ list, ...props }) {
   return (
-    <Scrollbar width={{ dt: '868' }}>
-      <Container {...props}>
-        {list.map((product, i) => {
-          if (i < 20) {
-            return (
-              <ProductsOrExercisesItem
-                key={product._id.$oid}
-                page="product"
-                data={product}
-              />
-            );
-          }
-          return null;
-        })}
-      </Container>
-    </Scrollbar>
+    <Container {...props}>
+      {list.map((product, i) => {
+        if (i < 20) {
+          return (
+            <ProductsOrExercisesItem
+              key={product._id.$oid}
+              page="product"
+              data={product}
+            />
+          );
+        }
+        return null;
+      })}
+    </Container>
   );
 }
 
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -3,6 +3,7 @@ import Title from '../../components/Title/Title';
 import ProductsFilter from '../../components/ProductsFilter/ProductsFilter';
 import { FlexWrapper, ProductPageContainer } from './Products.styled';
 import productsList from '../../data/products.json';
+import Scrollbar from '../../components/Scrollbar';
 
 const Products = () => {
   return (
@@ -11,7 +12,9 @@ const Products = () => {
         <Title text="Products" />
         <ProductsFilter />
       </FlexWrapper>
-      <ProductsOrExercisesContainer list={productsList} />
+      <Scrollbar width={{ dt: '868' }}>
+        <ProductsOrExercisesContainer list={productsList} />
+      </Scrollbar>
     </ProductPageContainer>
   );
 };
