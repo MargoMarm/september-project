a9da9d5c96a23eab5ff1ad976d35c2d1e6ad61f1
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-09-22 18:51:30 +0300

    update
    moved the modalka inside the list component.
    added a renderer of the component for the condition.

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -5,14 +5,11 @@ import { FlexWrapper, ProductPageContainer } from './Products.styled';
 import productsList from '../../data/products.json';
 import Scrollbar from '../../components/Scrollbar';
 import ProductsOrExercisesItem from '../../components/ProductsOrExercisesItem/ProductsOrExercisesItem';
-import Modal from '../../components/Modal/Modal';
-import AddProductForm from '../../components/AddProductForm/AddProductForm';
-import { useState } from 'react';
+// import Modal from '../../components/Modal/Modal';
+// import AddProductForm from '../../components/AddProductForm/AddProductForm';
+// import { useState } from 'react';
 
 const Products = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const toggleModal = () => setIsModalOpen(state => !state);
-
   return (
     <ProductPageContainer>
       <FlexWrapper>
@@ -24,20 +21,11 @@ const Products = () => {
           {productsList.map((product, i) => {
             if (i < 20) {
               return (
-                <>
-                  <ProductsOrExercisesItem
-                    key={product._id.$oid}
-                    page="product"
-                    data={product}
-                    openModal={toggleModal}
-                  />
-
-                  {isModalOpen && (
-                    <Modal openModal={toggleModal}>
-                      <AddProductForm closeModal={toggleModal} data={product} />
-                    </Modal>
-                  )}
-                </>
+                <ProductsOrExercisesItem
+                  key={product._id.$oid}
+                  page="product"
+                  data={product}
+                />
               );
             }
 
