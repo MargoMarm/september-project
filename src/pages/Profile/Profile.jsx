d06880883aa2c19bdd61ae747e2b37ece53b9886
Oleksandr <bragardlin@gmail.com>
2023-09-26 18:09:38 +0300

    fix submit

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -4,24 +4,47 @@ import UserCard from "../../components/UserCard";
 import UserForm from "../../components/UserForm";
 import { BlockWrapper, FormWrap, Container } from "./Profile.styled";
 import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../redux/auth/selectors";
 import { updateUserData } from "../../redux/auth/operation";
 
-const mgUserPage = {
-  top: { tab: 72, mob: 40 },
-  bt: { desk: 32, tab: 64, mob: 40 },
-};
 export default function Profile() {
   const [avatarFile, setAvatarFile] = useState();
   const dispatch = useDispatch()
+  const {
+    name,
+    bodyParameters: {
+      height,
+      currentWeight,
+      desiredWeight,
+      birthday,
+      blood,
+      sex,
+      levelActivity, 
+    }
+  } = useSelector(selectUser);
   
   const handleSubmit = (data) => {
+    if (data.name == name
+      && data.height == height
+      && data.currentWeight == currentWeight
+      && data.desiredWeight == desiredWeight
+      && data.birthday == birthday
+      && data.blood == blood
+      && data.sex == sex
+      && data.levelActivity == levelActivity
+      && !avatarFile) {
+      return
+    }
+
     const formData = new FormData();
     Object.entries(data).forEach(value => {
       formData.append(value[0], value[1])
     });
-    avatarFile && formData.append('avatar', avatarFile, avatarFile.name)
+    avatarFile && formData.append('avatar', avatarFile, avatarFile.name);
     
     dispatch(updateUserData(formData));
+    setAvatarFile(null);
   }
 
   return (
