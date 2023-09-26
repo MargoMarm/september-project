fb4b1a298aefb307f0a5a29f8b060fc45a06e469
MargoMarm <margogerasimenko@gmail.com>
2023-09-26 16:36:35 +0200

    add loader on page exercises

--- src/redux/exercises/selectors.js
+++ src/redux/exercises/selectors.js
@@ -1,2 +1,3 @@
 export const selectItems = state => state.exercises.items;
 export const selectGetFilters = state => state.exercises.getFilters;
+export const selectIsLoadingExercises = state => state.exercises.isLoading;
