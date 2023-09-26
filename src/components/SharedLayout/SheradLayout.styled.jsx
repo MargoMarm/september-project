fa515d30572d4fd8472fa03c9cb80c44dcda3c41
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-09-26 14:34:56 +0300

    delete some unnecessary trash

--- src/components/SharedLayout/SheradLayout.styled.jsx
+++ src/components/SharedLayout/SheradLayout.styled.jsx
@@ -4,7 +4,6 @@ import { mq } from '../../utils';
 
 export const Container = styled.div`
   margin: 0px auto;
-  ${'' /* padding: 0px 20px; */}
 
   ${mq.mobile} {
     width: 375px;
@@ -12,11 +11,9 @@ export const Container = styled.div`
 
   ${mq.tablet} {
     width: 768px;
-    ${'' /* padding: 0px 32px; */}
   }
 
   ${mq.desktop} {
     width: 1440px;
-    ${'' /* padding: 0px 96px; */}
   }
 `;
