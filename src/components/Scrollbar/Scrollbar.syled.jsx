709a36eb98160fb4607e97d25ac0527736747486
nzend <115723110+nzend@users.noreply.github.com>
2023-09-27 21:05:22 +0300

    Update Scrollbar.syled.jsx

--- src/components/Scrollbar/Scrollbar.syled.jsx
+++ src/components/Scrollbar/Scrollbar.syled.jsx
@@ -3,11 +3,13 @@ import { mq } from '../../utils';
 
 export const ScrollContainer = styled.div`
   width: ${({ width }) => width?.mob + 'px' || '100%'};
-  height: 100vh;
+  
   ${mq.tablet} {
     width: ${({ width }) => width?.nab + 'px' || '100%'};
+   
   }
   ${mq.desktop} {
     width: ${({ width }) => width?.dt + 'px' || '100%'};
+    height: calc(100vh - 220px);
   }
 `;
